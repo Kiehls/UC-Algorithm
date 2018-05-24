e6ed783e5ea7b0865c28a6f1e12306796cb6f12e
Unknown <nonestrike@gmail.com>
2018-05-24 18:23:58 +0900

    Delete trash

--- Algorithm/W 문제해결 기본] 4일차 - 거듭 제곱.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  W 문제해결 기본] 4일차 - 거듭 제곱.cpp
-//  Algorithm
-//
-//  Created by Seungsoo on 4/26/18.
-//  Copyright © 2018 Seungsoo. All rights reserved.
-//
-
-#include <stdio.h>
